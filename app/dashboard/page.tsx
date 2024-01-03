b4fc64188056266cf56a2f9e7755bca14de539ad
Emre Cakiroglu <y.emrecakiroglu@protonmail.com>
2024-01-04 02:34:10 +0300

    chore: update title and desc

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -20,8 +20,7 @@ export default async function Dashboard() {
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       <div className="animate-in flex-1 flex flex-col gap-20 opacity-0 max-w-4xl px-3">
         <main className="flex-1 flex flex-col gap-6">
-          <h2 className="font-bold text-4xl mb-4">Next steps</h2>
-          <SignUpUserSteps />
+          <h2 className="font-bold text-4xl mb-4">Welcome back!</h2>
         </main>
       </div>
     </div>
